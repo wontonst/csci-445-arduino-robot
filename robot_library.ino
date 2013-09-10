33a2f6706e9347452642037d5dcdafbb285a3120
YiWei Roy Zheng <roycraft3@gmail.com>
2013-09-10 14:55:11 -0700

    Update robot_library.ino

--- robot_library.ino
+++ robot_library.ino
@@ -20,67 +20,88 @@ int current_velocity = 0;
  
 void loop () {
     turnLeftAdvanced(0);
-    /*forward(2000);
-  stop(1500);
-    reverse(2000);
-    stop(1500);
-    turnRight(2000);
-    stop(1500);
-    turnLeft(5000);
-    stop(1500);*/
+    c_forward(2000);
+  c_brakeAll(1500);
+    c_reverse(2000);
+    c_brakeAll(1500);
+    c_turnRight(2000);
+    c_brakeAll(1500);
+    c_turnLeft(5000);
+    c_brakeAll(1500);
 }
 
-void stop(int time){
-    breakLeft(time);
-    breakRight(time);
+void s_brakeAll(){
+    s_brakeLeft();
+    s_brakeRight();
 }
- 
-void forward(int time){
+ void c_brakeAll(int time){
+   s_brakeAll();
+   delay(time);
+ }
+void s_forward(){
     digitalWrite(motorpinRR, HIGH);
     digitalWrite(motorpinRL, LOW);
     digitalWrite(motorpinLR, HIGH);
     digitalWrite(motorpinLL,LOW);
+}
+void c_forward(int time){
+  s_forward();
     delay(time);
 }
  
-void reverse(int time){
+void s_reverse(){
     digitalWrite(motorpinRR, LOW);
     digitalWrite(motorpinRL, HIGH);
     digitalWrite(motorpinLR, LOW);
     digitalWrite(motorpinLL,HIGH);
-    delay(time);
 }
- 
-void turnRight(int time)
+ void c_reverse(int time){
+   s_reverse();
+    delay(time);
+ }
+void s_turnRight()
 {
     digitalWrite(motorpinRR, HIGH);
     digitalWrite(motorpinRL, LOW);
     digitalWrite(motorpinLR, LOW);
     digitalWrite(motorpinLL,HIGH);
-    delay(time);
 }
- 
-void turnLeft(int time)
+ void c_turnRight(int time){
+   s_turnRight();
+    delay(time);
+ }
+void s_turnLeft()
 {
     digitalWrite(motorpinRR, LOW);
     digitalWrite(motorpinRL, HIGH);
     digitalWrite(motorpinLR, HIGH);
     digitalWrite(motorpinLL,LOW);
-    delay(time);
 }
-void breakLeft(int time){
+void c_turnLeft(int time){
+  s_turnLeft();
+delay(time);
+}
+void s_brakeLeft(){
     digitalWrite(motorpinLR,LOW);
     digitalWrite(motorpinLL,LOW);
-    delay(time);
 }
-void breakRight(int time){
+void s_brakeRight(){
     digitalWrite(motorpinRR,LOW);
     digitalWrite(motorpinRL,LOW);
+}
+void c_brakeLeft(int time){
+  s_brakeLeft();
+  delay(time);
+}
+void c_brakeRight(int time){
+  s_brakeRight();
   delay(time);
 }
 void turnLeftAdvanced(int time){
   for(int i = 0 ;i  != 10; i++){
-  forward(100);
-  turnLeft(100);
+  c_forward(100);
+  c_turnLeft(100);
+  time -= 200;
+  if(time <= 0)return;
   }
 }
