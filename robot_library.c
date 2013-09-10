046890bc23e05cd963a6400652cb632d28af8e16
YiWei Roy Zheng <roycraft3@gmail.com>
2013-09-10 14:46:27 -0700

    Update robot_library.c

--- robot_library.c
+++ robot_library.c
@@ -19,25 +19,20 @@ void setup () {
 int current_velocity = 0;
  
 void loop () {
-    
-    forward(2000);
-    stop(1500);
+    turnLeftAdvanced(0);
+    /*forward(2000);
+  stop(1500);
     reverse(2000);
     stop(1500);
     turnRight(2000);
     stop(1500);
-    turnLeft(2000);
-    stop(1500);
+    turnLeft(5000);
+    stop(1500);*/
 }
 
-
-
 void stop(int time){
-    digitalWrite(motorpinRR,LOW);
-    digitalWrite(motorpinRL,LOW);
-    digitalWrite(motorpinLR,LOW);
-    digitalWrite(motorpinLL,LOW);
-    delay(time);
+    breakLeft(time);
+    breakRight(time);
 }
  
 void forward(int time){
@@ -62,6 +57,7 @@ void turnRight(int time)
     digitalWrite(motorpinRL, LOW);
     digitalWrite(motorpinLR, LOW);
     digitalWrite(motorpinLL,HIGH);
+    delay(time);
 }
  
 void turnLeft(int time)
@@ -71,4 +67,20 @@ void turnLeft(int time)
     digitalWrite(motorpinLR, HIGH);
     digitalWrite(motorpinLL,LOW);
     delay(time);
+}
+void breakLeft(int time){
+    digitalWrite(motorpinLR,LOW);
+    digitalWrite(motorpinLL,LOW);
+    delay(time);
+}
+void breakRight(int time){
+    digitalWrite(motorpinRR,LOW);
+    digitalWrite(motorpinRL,LOW);
+  delay(time);
+}
+void turnLeftAdvanced(int time){
+  for(int i = 0 ;i  != 10; i++){
+  forward(100);
+  turnLeft(100);
+  }
 }
