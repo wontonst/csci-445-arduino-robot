50f294607d3b2c9b5a7fc6c2e861e29393c958d3
Yiwei Zheng <yiweizhe@usc.edu>
2013-10-01 13:05:38 -0700

    format

--- robot_library.ino
+++ robot_library.ino
@@ -1,24 +1,25 @@
 #include <robot.h>
- 
+
 Robot* robot = new Robot();
- 
+
 void setup () {
-    robot = new Robot();
-    robot->setup();
+  robot = new Robot();
+  robot->setup();
 }
- 
+
 void loop () {
   robot->followWall()
-}
-  
-void diagnostic(){
-  
-  robot->forward(2000);
-robot->brakeAll(1500);
-robot->reverse(1500);
-robot->brakeAll(1500);
-robot->turnRight(2000);
-robot->brakeAll(1500);
-robot->turnLeft(1500);
-robot->brakeAll(1500);
-}
+  }
+
+  void diagnostic(){
+
+    robot->forward(2000);
+    robot->brakeAll(1500);
+    robot->reverse(1500);
+    robot->brakeAll(1500);
+    robot->turnRight(2000);
+    robot->brakeAll(1500);
+    robot->turnLeft(1500);
+    robot->brakeAll(1500);
+  }
+
